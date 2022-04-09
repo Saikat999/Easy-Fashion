97a7bb758c48c337f6ef9f0ca615441690c7f7af
Saikat Hasan <saikathasan005@gmail.com>
2022-04-09 22:58:23 +0600

    Send data to admin table through postman

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+     public function create(Request $req){
+
+        $admin =new Admin;
+        $admin->name = $req->input('name');
+        $admin->email = $req->input('email');
+        $admin->password =Hash::make( $req->input('password'));
+        $admin->save();
+        return $admin;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/admin', [AdminController::class, 'create']);
